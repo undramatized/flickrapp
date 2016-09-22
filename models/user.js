fc1d14afea8f78e533b96e1580a3a19b88aa04bb
Rama Krishna <siddarth21@hotmail.com>
2016-09-22 10:49:01 +0800

    adding sessions

--- models/user.js
+++ models/user.js
@@ -1,13 +1,56 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
-    email: DataTypes.STRING,
-    name: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail:{
+          msg: "Invalid Email Address"
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,99],
+          msg: "Name must be between 1 to 99 characters"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: "Password must be at least 8 characters"
+        }
+      }
+    }
   }, {
+    hooks:{
+      beforeCreate: function(newUser, options, cb){
+        var hash = bcrypt.hashSync(newUser.password, 10);
+        newUser.password = hash;
+        cb(null, newUser);
+      }
+    },
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      validPassword: function(pw){
+        return bcrypt.compareSync(pw, this.password);
+      },
+      toJSON: function(){
+        var user = this.get();
+        delete user.password;
+        return user;
       }
     }
   });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PhotoCatch' });
+});
+
+router.get('/signup', function(req, res){
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res) {
+  // find or create a user, providing the name and password as default values
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      // if created, success and redirect home
+      console.log('User created!');
+      res.redirect('/');
+    } else {
+      // if not created, the email already exists
+      console.log('Email already exists');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    // if an error occurs, let's see what the error is
+    console.log('An error occurred: ', error.message);
+    res.redirect('/auth/signup');
+  });
+});
+
+router.post('/login', function(req, res){
+  res.render('login');
 });
 
 module.exports = router;
